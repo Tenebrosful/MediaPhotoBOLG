eae57f35c5145a4a4c04c73c5611bd2bef695690
Tenebrosful <hugobernard39@gmail.com>
2020-11-06 02:06:10 +0100

    Ajout du pivot ImageGalerie.php

---
+++ src/Domain/Pivots/ImageGalerie.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App\Domain\Pivots;
+
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class ImageGalerie extends Pivot
+{
+    /**
+     * Nom de la table
+     */
+    protected $table = 'imagegalerie';
+
+    /**
+     * Nom de la primary key
+     */
+    protected $primaryKey = [
+        'id_galerie',
+        'id_image'
+    ];
+
+    protected $incrementing = false;
+
+    /**
+     * Liste des colones modifiables
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id_galerie',
+        'id_image'
+    ];
+
+    /**
+     * Liste des colones à cacher en cas de conversion en String / JSON
+     *
+     * @var array
+     */
+    protected $hidden = [];
+}
