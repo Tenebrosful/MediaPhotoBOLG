e7805345992ebf09e0bda79e9c872e69c8bffd91
Tenebrosful <hugobernard39@gmail.com>
2020-11-06 01:43:41 +0100

    Création de la table Galerie

---
+++ src/Domain/Galerie/Galerie.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Domain\Galerie;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Galerie extends Model
+{
+    /**
+     * Nom de la table
+     */
+    protected $table = 'galerie';
+
+    /**
+     * Nom de la primary key
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Liste des colones modifiables
+     *
+     * @var array
+     */
+    protected $fillable = ['nom', 'description', 'id_owner', 'isPrivate'];
+
+    /**
+     * Liste des colones à cacher en cas de conversion en String / JSON
+     *
+     * @var array
+     */
+    protected $hidden = [];
+}
