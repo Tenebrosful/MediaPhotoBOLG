6399198e2682487c84ad9805f4119165689ce09c
Tenebrosful <hugobernard39@gmail.com>
2020-11-06 02:08:14 +0100

    Ajout du pivot UserGalerie.php

---
+++ src/Domain/Pivots/UserGalerie.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App\Domain\Pivots;
+
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class UserGalerie extends Pivot
+{
+    /**
+     * Nom de la table
+     */
+    protected $table = 'usergalerie';
+
+    /**
+     * Nom de la primary key
+     */
+    protected $primaryKey = [
+        'id_user',
+        'id_galerie'
+    ];
+
+    protected $incrementing = false;
+
+    /**
+     * Liste des colones modifiables
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id_user',
+        'id_galerie'
+    ];
+
+    /**
+     * Liste des colones à cacher en cas de conversion en String / JSON
+     *
+     * @var array
+     */
+    protected $hidden = [];
+}
