691511279cdabee5e633d05827859a5cb308abfc
Gardel Valentin <valentin.gardel2@etu.univ-lorraine.fr>
2020-11-07 22:45:36 +0100

    add ListGalerieAction

---
+++ src/Application/Actions/Galerie/ListGalerieAction.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Application\Actions\Galerie;
+
+
+use App\Application\Actions\Action;
+use App\Domain\DomainException\DomainRecordNotFoundException;
+use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Exception\HttpBadRequestException;
+
+class ListGalerieAction extends Action
+{
+
+    /**
+     * @return Response
+     * @throws DomainRecordNotFoundException
+     * @throws HttpBadRequestException
+     */
+    protected function action(): Response
+    {
+        return $this->view->render($this->response, 'ListGalerie.twig', null);
+    }
+}
