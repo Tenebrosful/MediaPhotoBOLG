76c07c4d7ae6ad6ff77b8c7a65cb841c92c51f87
Gardel Valentin <valentin.gardel2@etu.univ-lorraine.fr>
2020-11-07 22:44:18 +0100

    add slim routes

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 use App\Application\Actions\User\ListUsersAction;
 use App\Application\Actions\User\ViewUserAction;
+
+use App\Application\Actions\Galerie\ListGalerieAction;
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
@@ -22,5 +25,27 @@ return function (App $app) {
     $app->group('/users', function (Group $group) {
         $group->get('', ListUsersAction::class);
         $group->get('/{id}', ViewUserAction::class);
+    });
+
+    $app->get('/home', HomeAction::class);
+
+    $app->get('/profil', ViewProfilAction::class);
+    $app->post('/profil', UpdateProfilAction::class);
+
+    $app->group("/galeries", function(Group $group) {
+        $group->get('', ListGalerieAction::class);
+        $group->get('/{id}', ViewGalerieAction::class);
+
+        $group->get('/{id}/settings', SettingGalerieAction::class);
+        $group->post('/{id}/settings', UpdateGalerieAction::class);
+        $group->delete('/{id}/settings', DeleteGalerieAction::class);
+
+        $group->get('/{id}/page/{page}', ListGaleriePhotoAction::class);
+
+        $group->get('/{id}/photo/{photo}', ViewPhotoAction::class);
+
+        $group->get('/{id}/photo/{photo}/settings', ViewPhotoAction::class);
+        $group->post('/{id}/photo/{photo}/settings', UpdatePhotoAction::class);
+        $group->delete('/{id}/photo/{photo}/settings', DeletePhotoAction::class);
     });
 };
