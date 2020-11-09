c07fcedc5cb1ea682ebfd0d1a610e34ba4a3cb75
Gardel Valentin <valentin.gardel2@etu.univ-lorraine.fr>
2020-11-09 17:40:26 +0100

    fiw bdd connection

--- public/index.php
+++ public/index.php
@@ -11,6 +11,21 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$capsule = new Capsule;
+
+if (is_array($bddConfig = parse_ini_file(__DIR__.'/../config/bdd.ini')))
+    $capsule->addConnection($bddConfig);
+else {
+    echo "ERREUR : Fichier de configuration de la base de donnée introuvable";
+    exit();
+}
+
+// Make this Capsule instance available globally
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM.
+$capsule->bootEloquent();
+
 // Instantiate PHP-DI ContainerBuilder
 $containerBuilder = new ContainerBuilder();
 
@@ -66,24 +81,9 @@ $app->addRoutingMiddleware();
 
 // Add Error Middleware
 $errorMiddleware = $app->addErrorMiddleware($displayErrorDetails, true, true);
-$errorMiddleware->setDefaultErrorHandler($errorHandler);
+//$errorMiddleware->setDefaultErrorHandler($errorHandler);
 
 // Run App & Emit Response
 $response = $app->handle($request);
 $responseEmitter = new ResponseEmitter();
 $responseEmitter->emit($response);
-
-$capsule = new Capsule;
-
-if (is_array($bddConfig = parse_ini_file(__DIR__.'/../config/bdd.ini')))
-    $capsule->addConnection($bddConfig);
-else {
-    echo "ERREUR : Fichier de configuration de la base de donnée introuvable";
-    exit();
-}
-
-// Make this Capsule instance available globally
-$capsule->setAsGlobal();
-
-// Setup the Eloquent ORM.
-$capsule->bootEloquent();
